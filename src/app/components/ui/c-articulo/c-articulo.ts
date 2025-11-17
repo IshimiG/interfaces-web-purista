d8e90211ff19de2388aeb983d615328cdddf02d0
Gael Yasunari Ishimi <yasunarishimi@gmail.com>
2025-11-17 13:23:29 +0100

    important g-classes

--- src/app/components/ui/c-articulo/c-articulo.ts
+++ src/app/components/ui/c-articulo/c-articulo.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, HostBinding, Input, OnInit, Output} from '@angular/core';
 import {CommonModule} from '@angular/common';
  
 @Component({
@@ -7,14 +7,29 @@ import {CommonModule} from '@angular/common';
   templateUrl: './c-articulo.html',
   styleUrl: './c-articulo.scss'
 })
-export class CArticulo  {
+export class CArticulo implements OnInit  {
   @Input() nombre:string="";
   @Input() urlImagen:string="";
   @Input() precioActual:number=0;
   @Input() precioAnterior:number=0;
   @Input() valoracion:number=0;
   @Input() numeroOpiniones:number=0;
-  @Input() fechaEntrega:Date=new Date();
+  @Input() fechaEntrega!:Date | string;
   @Input() precioEnvio:number=0;
   @Input() otrosEnvios:boolean=false;
+  @Input() articleBackgroundColor!: 'dark' | 'light';
+  @Input() articleWidth!: 'small' | 'medium' | 'large';
+
+  @HostBinding('class')
+  get Clazz(): Record<string, boolean> {
+    return {
+      'g--background-color-gris-1': this.articleBackgroundColor === 'dark',
+      'g--background-color-blanco-1': this.articleBackgroundColor === 'light',
+      'g--width-1': this.articleWidth ==='small'
+    }
+  }
+
+  ngOnInit() {
+    this.fechaEntrega = new Date(this.fechaEntrega);
+  }
 }
